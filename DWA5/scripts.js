d44b30a970c74296dc1328a6d536daf30ba9e233
TshepiMore <125869424+TshepiMore@users.noreply.github.com>
2023-06-06 16:00:02 +0200

    DWA5

---
+++ DWA5/scripts.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+
+ // Checking if either dividend or divider is falsy,displaying an error message if either value is missing.
+    if (!dividend || !divider) {
+        result.innerText = "Division not performed. Both values are required in inputs. Try again";
+  }
+
+  // Checking if divider is less than 0, if so display error message and console error.
+    else if (divider < 0) {
+        result.innerText = "Division not performed. Invalid number provided. Try again";
+        console.error("Invalid division:", dividend, "/", divider);
+  }
+
+   // Checking if either dividend or divider is not a number, if so display error message and console error.
+     else if ( isNaN(dividend) || isNaN(divider)){
+        document.body.innerHTML = "Something critical went wrong. Please reload the page";
+        console.error("Critical error occured.");
+  }
+
+  // Performing the division and rounding down to the nearest whole number.
+    else {
+        const divisionResult = Math.floor(dividend / divider);
+        result.innerText = divisionResult;
+  }
+});
